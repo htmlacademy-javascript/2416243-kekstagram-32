1ee4467b766d725c5c8004a95554c5a262be7675
Sergei <red00009@outlook.com>
2024-07-01 19:21:55 -0400

    module 5 task 2

--- js/functions.js
+++ js/functions.js
@@ -1,56 +1,23 @@
-// Длина строки
-const checkStringLength = (string, lengthNumber) => string.length <= lengthNumber;
+const convertTimeIntoMinutes = (time) => {
+  const minutesInHour = 60;
+  const [hours, minutes] = time.split(':').map((item) => Number(item));
+
+  return hours * minutesInHour + minutes;
+};
+
+const isMeetingInsideBusinessHours = (startWorkHour, endWorkHour, startMeetingHour, meetingDuration) => {
+  const startWorkHourInMinutes = convertTimeIntoMinutes(startWorkHour);
+  const endWorkHourInMinutes = convertTimeIntoMinutes(endWorkHour);
+  const startMeetingHourInMinutes = convertTimeIntoMinutes(startMeetingHour);
+  const endMeetingHourInMinutes = startMeetingHourInMinutes + meetingDuration;
+
+  return startMeetingHourInMinutes >= startWorkHourInMinutes && endMeetingHourInMinutes <= endWorkHourInMinutes;
+};
 
 /* eslint-disable */
-console.log(`Длина проверяемой строки меньше или равна 20 - ${checkStringLength('проверяемая строка', 20)}`);
-console.log(`Длина проверяемой строки меньше или равна 18 - ${checkStringLength('проверяемая строка', 18)}`);
-console.log(`Длина проверяемой строки меньше или равна 10 - ${checkStringLength('проверяемая строка', 10)}`);
-/* eslint-enable */
-
-// Палиндром
-function isStringPalindrome(string) {
-  const trimmedString = string.replaceAll(/[^а-яА-Яa-zA-Z0-9]/g, '').toLowerCase();
-
-  let reversedString = '';
-  for (let i = trimmedString.length - 1; i >= 0; i--) {
-    reversedString += trimmedString[i];
-  }
-
-  return trimmedString === reversedString;
-}
-
-/* eslint-disable */
-console.log(`Проверяемая строка является палиндромом - ${isStringPalindrome('топот')}`);
-console.log(`Проверяемая строка является палиндромом - ${isStringPalindrome('ДовОд')}`);
-console.log(`Проверяемая строка является палиндромом - ${isStringPalindrome('Кекс')}`);
-console.log(`Проверяемая строка является палиндромом - ${isStringPalindrome('Лёша на полке клопа нашёл ')}`);
-/* eslint-enable */
-
-// Получить число из строки
-function extractNumber(string) {
-  if (typeof string === 'number') {
-    string = string.toString();
-  }
-
-  let number = '';
-  for (const char of string) {
-    if (!Number.isNaN(parseInt(char, 10))) {
-      number += char;
-    }
-  }
-
-  return number
-    ? parseInt(number, 10)
-    : NaN;
-}
-
-/* eslint-disable */
-console.log(extractNumber('2023 год'));
-console.log(extractNumber('ECMAScript 2022'));
-console.log(extractNumber('1 кефир, 0.5 батона'));
-console.log(extractNumber('агент 007'));
-console.log(extractNumber('а я томат'));
-console.log(extractNumber(2023));
-console.log(extractNumber(-1));
-console.log(extractNumber(1.5));
+console.log(isMeetingInsideBusinessHours('08:00', '17:30', '14:00', 90)); // true
+console.log(isMeetingInsideBusinessHours('8:0', '10:0', '8:0', 120)); // true
+console.log(isMeetingInsideBusinessHours('08:00', '14:30', '14:00', 90)); // false
+console.log(isMeetingInsideBusinessHours('14:00', '17:30', '08:0', 90)); // false
+console.log(isMeetingInsideBusinessHours('8:00', '17:30', '08:00', 900)); // false
 /* eslint-enable */
